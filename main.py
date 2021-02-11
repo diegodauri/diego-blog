3cec6963b21b766b26a25c2a6f677ca47c69b136
diegodauri <diegodauri@users.noreply.github.com>
2021-02-11 14:47:40 +0100

    Add default environment variables

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ import smtplib
 import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY", "keyboardcat")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 login_manager = LoginManager()
@@ -21,7 +21,7 @@ login_manager.init_app(app)
 gravatar = Gravatar(app)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:///blog.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
